06671fd2d5c3c8ff3125aafda48368fdadc13c5d
장효원 <hyo961015@naver.com>
2022-01-02 14:45:55 +0900

    init

---
+++ JAlert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JAlert
+//
+//  Created by 장효원 on 2022/01/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
