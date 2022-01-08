72c8b881c6ad926b157083d125e5815df4c54a76
장효원 <hyo961015@naver.com>
2022-01-08 21:27:46 +0900

    Add UITableView

--- Example/JAlert/ViewController.swift
+++ Example/JAlert/ViewController.swift
@@ -10,20 +10,54 @@ import UIKit
 import JAlert
 
 class ViewController: UIViewController {
-
+    @IBOutlet var tableView: UITableView!
+    
+    var data:[String] = ["Default"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        let view = JAlert(title: "알림", message: "내용", alertType: .default)
-        view.show()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        setupUI()
     }
     
+    func setupUI() {
+        registerDelegate()
+    }
 
+    func registerDelegate() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ExampleListCell", for: indexPath) as! ExampleListCell
+        let index = indexPath.row
+        
+        cell.titleLabel.text = data[index]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let index = indexPath.row
+        
+        switch index {
+        case 0:
+            let alert = JAlert(title: "Title", message: "Content", alertType: .default)
+            alert.show()
+        default:
+            print("the rest of index")
+        }
+    }
+}
+
+class ExampleListCell: UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    
+}
