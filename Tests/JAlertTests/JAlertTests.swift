379aaef474f12c657c9488cd6eb5494325e7703a
JacksonJang <hyo961015@naver.com>
2022-02-16 20:55:37 +0900

    Add SPM and Update 0.6.0

---
+++ Tests/JAlertTests/JAlertTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import JAlert
+
+final class JAlertTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(JAlert().text, "Hello, World!")
+    }
+}
