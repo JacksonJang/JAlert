630059491b5b6f8b7d22bba31378370251e6d761
JacksonJang <hyo961015@naver.com>
2022-02-16 21:02:49 +0900

    Update 0.6.1 and Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "JAlert",
-            dependencies: []),
+            path: "Sources"),
         .testTarget(
             name: "JAlertTests",
             dependencies: ["JAlert"]),
