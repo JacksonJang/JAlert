3852aae12bc8b1594de46fc99683404ac3c7b895
JacksonJang <hyo961015@naver.com>
2022-05-29 10:24:31 +0900

    Fixed Error

--- Sources/JLabelStackView.swift
+++ Sources/JLabelStackView.swift
@@ -11,7 +11,7 @@ class JLabelStackView: UIStackView {
         isLayoutMarginsRelativeArrangement = true
     }
     
-    required init?(coder: NSCoder) {
+    required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
